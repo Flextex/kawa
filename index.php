c3050e41eca9451ab837d0f7fe0574b8342dda99
Flextex <66634323+Flextex@users.noreply.github.com>
2020-07-10 18:13:00 -0400

    ss

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -12,19 +9,7 @@
 <body>
 <!-- partial:index.partial.html -->
 <div class="vn-red" style="text-align: center;">
-<a href="generate-link.php?gmexsite">DOWNLOAD</a>
-</div>
-<div class="vn-green" style="text-align: center;">
-<a href="#" class="btn-wrap">DOWNLOAD</a>
-</div>
-<div class="vn-teal" style="text-align: center;">
-<a href="#" class="btn-wrap">DOWNLOAD</a>
-</div>
-<div class="vn-orange" style="text-align: center;">
-<a href="#" class="btn-wrap">DOWNLOAD</a>
-</div>
-<div class="vn-blue" style="text-align: center;">
-<a href="#" class="btn-wrap">DOWNLOAD</a>
+<a href="generate-boton.php?gmexsite">DOWNLOAD</a>
 </div>
 <!-- partial -->
   
@@ -32,71 +17,3 @@
 </html>
 
 
-
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>CodePen - App download button concept</title>
-  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Varela+Round'>
-<link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'><link rel="stylesheet" href="./style.css">
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<a class="download" href="#platforms"></a>
-<div class="platforms" id="platforms">
-	<a href="generate-link.php?gmexsite" data-os="windows"></a>
-	<a href="#mac" data-os="mac"></a>
-	<a href="#linux" data-os="linux"></a>
-</div>
-<div class="installer" id="windows">
-	<div class="info" data-weight="753 KB"></div>
-	<div class="details">
-		<p>Windows installer</p>
-		<span>Version 1.0.6</span>
-		<ul>
-			<li>List item number one</li>
-			<li>List item number two</li>
-			<li>List item number three</li>
-			<li>List item number four</li>		
-		</ul>
-	</div>
-	<label for="progressWindows"><input type="radio" id="progressWindows"/><span></span></label>
-	<a class="close" href="download-link.php?"></a>
-</div>
-<div class="installer" id="mac">
-	<div class="info" data-weight="634 KB"></div>
-	<div class="details">
-		<p>Mac installer</p>
-		<span>Version 1.0.6</span>
-		<ul>
-			<li>List item number one</li>
-			<li>List item number two</li>
-			<li>List item number three</li>
-			<li>List item number four</li>		
-		</ul>
-	</div>
-	<label for="progressMac"><input type="radio" id="progressMac"/><span></span></label>
-	<a class="close" href="#"></a>
-</div>
-<div class="installer" id="linux">
-	<div class="info" data-weight="781 KB"></div>
-	<div class="details">
-		<p>Linux installer</p>
-		<span>Version 1.0.6</span>
-		<ul>
-			<li>List item number one</li>
-			<li>List item number two</li>
-			<li>List item number three</li>
-			<li>List item number four</li>		
-		</ul>
-	</div>
-	<label for="progressLinux"><input type="radio" id="progressLinux"/><span></span></label>
-	<a class="close" href="#"></a>
-</div>
-<!-- partial -->
-  <script  src="./script.js"></script>
-
-</body>
-</html>
