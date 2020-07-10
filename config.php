68303ac614565553392bd56bb1ac7d68d9bdfb43
Flextex <66634323+Flextex@users.noreply.github.com>
2020-07-10 16:59:05 -0400

    ssss

--- config.php
+++ config.php
@@ -28,4 +28,4 @@ define('TOKEN_DIR', 'tokens');
 define('OAUTH_PASSWORD','gmexsite');
 
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
-define('EXPIRATION_TIME', '+60 minutes');
+define('EXPIRATION_TIME', '+1 year');
