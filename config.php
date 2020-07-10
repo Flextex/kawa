45687e71d7fad3c179f89c4e416d3324c9986a74
Flextex <66634323+Flextex@users.noreply.github.com>
2020-07-10 18:29:09 -0400

    sss

--- config.php
+++ config.php
@@ -28,4 +28,4 @@ define('TOKEN_DIR', 'tokens');
 define('OAUTH_PASSWORD','NORP');
 
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
-define('EXPIRATION_TIME', '+24 hours');
+define('EXPIRATION_TIME', '+1 year');
